4f28518267e93bf371380df8ac86ba3974459e81
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2020-07-28 04:35:36 -0300

    add cors and list projects from api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import api from './services/api';
 
 import './App.css';
@@ -6,17 +6,27 @@ import './App.css';
 import Header from './components/Header'
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de App', 'Front-End']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data);
+    })
+  }, [])
 
   function handleAddProject() {
-    setProjects([...projects, `Novo Projeto criado em: ${Date.now()}`]);
+    setProjects([...projects, {
+      id: Date.now(),
+      title: `Novo Projeto Criado em ${Date.now()}`,
+      owner: 'Ebraim Carvalho'
+    }]);
   }
 
   return (
     <>
       <Header title='Empresa Kether!' />
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
       <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
     </>
