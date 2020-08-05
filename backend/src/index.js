c7e556af053cfbd62ca9f13b9cbe20d5a763cf5e
Ebraim Carvalho <ebraimcarvalho@gmail.com>
2020-08-04 21:08:31 -0300

    init mobile react native

--- backend/src/index.js
+++ backend/src/index.js
@@ -81,129 +81,64 @@ app.listen(3333, () => {
   console.log('💻 Back-end started!');
 });
 
+
 // const express = require('express');
-// const {uuid} = require('uuidv4');
+// const cors = require('cors');
+// const uuid = require('uuidv4');
+
 // const app = express();
 // app.use(express.json());
+// app.use(cors());
 
 // const projects = [];
 
 // app.get('/projects', (req, res) => {
-// 	const {title} = req.query;
-// 	const results = title 
-// 		? projects.filter(project => project.title.includes(title))
-// 		: projects;
-// 	return res.json(results);
+//   const {title} = req.query;
+//   const result = title ? projects.filter(project => project.title.includes(title)) : projects;
+
+//   return res.json(result);
 // })
 
 // app.post('/projects', (req, res) => {
-// 	const {title, owner} = req.body;
-// 	const project = {
-// 		id: uuid(),
-// 		title,
-// 		owner
-// 	}	
-
-// 	projects.push(project);
-
-// 	return res.json(project);
+//   const {title, owner} = req.body;
+//   const project = {
+//     id: uuid(),
+//     title,
+//     owner
+//   }
+//   projects.push(project);
+//   return res.json(project);
 // })
 
 // app.put('/projects/:id', (req, res) => {
-// 	const {id} = req.params;
-// 	const {title, owner} = req.body;
-// 	const projectIndex = projects.findIndex(project => project.id === id);
+//   const {id} = req.params;
+//   const {title, owner} = req.body;
+//   const projectIndex = projects.findIndex(project => project.id === id);
 
-// 	if(projectIndex < 0) {
-// 		return res.status(400).json({Error: "Project not found."});
-// 	}
+//   if(projectIndex < 0) {
+//     return res.status(400).json({ Erro: 'Project not found.'})
+//   }
 
-// 	const project = {
-// 		id,
-// 		title,
-// 		owner
-// 	}
+//   const project = {
+//     id,
+//     title,
+//     owner
+//   }
 
-// 	projects[projectIndex] = project;
+//   projects[projectIndex] = project;
 
-// 	return res.json(project);
+//   return res.json(project)
 // })
 
 // app.delete('/projects/:id', (req, res) => {
-// 	const {id} = req.params;
-// 	const projectIndex = projects.findIndex(project => project.id === id);
+//   const {id} = req.params;
+//   const projectIndex = projects.findIndex(project => project.id === id);
+//   if(projectIndex < 0) {
+//     return res.status(400).json({ Erro: 'Project not found.'});
+//   }
 
-// 	if(projectIndex < 0) {
-// 		return res.status(400).json({ Error: "Project not found." });
-// 	}
-
-// 	projects.splice(projectIndex, 1);
-
-// 	return res.status(204).send();	
+//   projects.splice(projectIndex, 1)
+//   return res.status(204).send()
 // })
 
-// app.listen(3333, () => {
-// 	console.log('Back-end started');
-// })
-
-// const express = require('express');
-// const {uuid} = require('uuidv4');
-// const app = express();
-// app.use(express.json());
-
-// const projects = [];
-
-// app.get('/projects', (req, res) => {
-// 	const {title} = req.query;
-// 	const results = title
-// 		? projects.filter(project => project.title.includes(title))
-// 		: projects;
-// 	return res.json(results);
-// })
-
-// app.post('/projects', (req, res) => {
-// 	const {title, owner} = req.body;
-// 	const project = {
-// 		id: uuid(),
-// 		title,
-// 		owner
-// 	}
-// 	projects.push(project);
-// 	return res.json(project);
-// })
-
-// app.put('/projects/:id', (req, res) => {
-// 	const {id} = req.params;
-// 	const {title, owner} = req.body;
-// 	const projectIndex = projects.findIndex(project => project.id === id);
-
-// 	if(projectIndex < 0) {
-// 		return res.status(404).json({Error: "Project not found."});
-// 	}
-
-// 	const project = {
-// 		id,
-// 		title,
-// 		owner
-// 	}
-
-// 	projects[projectIndex] = project;
-	
-// 	return res.json(project);
-// })
-
-// app.delete('/projects/:id', (req, res) => {
-// 	const {id} = req.params;
-// 	const projectIndex = projects.findIndex(project => project.id === id);
-
-// 	if(projectIndex < 0) {
-// 		return res.status(404).json({Error: "Project not found."})
-// 	}
-
-// 	projects.splice(projectIndex, 1);
-// 	return res.status(204).send();
-// })
-
-// app.listen(3333, () => {
-// 	console.log('Back-end started!')
-// })
+// app.listen(3333);
